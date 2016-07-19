6e5c961785fde0491dd5c7c90f4461855dbc161d
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-07-19 22:45:04 +0100

    Completed challenge 20

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,15 +1,23 @@
 
 feature "For new players to enter battle" do
-  scenario 'fill in form' do
-    player_1 = "Mannie"
-    player_2 = "Giancarlo"
+  player_1 = "Mannie"
+  player_2 = "Giancarlo"
 
+  scenario 'fill in form' do
     visit "/"
-    fill_in "player_1", :with => player_1
-    fill_in "player_2", :with => player_2
+    fill_in :player_1, :with => player_1
+    fill_in :player_2, :with => player_2
 
     click_button "Enter Battle"
 
     expect(page).to have_text("#{player_1} vs #{player_2}")
+  end
+
+  scenario 'players can see hit points' do
+    visit '/'
+    fill_in :player_1, :with => player_1
+    fill_in :player_2, :with => player_2
+    click_button 'Enter Battle'
+    expect(page).to have_text("#{player_2}: 30 Hit Points")
   end
 end
