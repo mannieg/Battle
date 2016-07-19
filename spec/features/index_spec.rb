6ae987e66edaf7d5bc04c11b35ab8045fd128e99
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-07-19 20:00:41 +0100

    Refactored html

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,6 +1,5 @@
-#require 'spec_helper'
 
-feature "For players to enter battle" do
+feature "For new players to enter battle" do
   scenario 'fill in form' do
     player_1 = "Mannie"
     player_2 = "Giancarlo"
@@ -11,6 +10,6 @@ feature "For players to enter battle" do
 
     click_button "Enter Battle"
 
-    expect(page).to have_text("Welcome to the battle #{player_1} and #{player_2}")
+    expect(page).to have_text("#{player_1} vs #{player_2}")
   end
 end
